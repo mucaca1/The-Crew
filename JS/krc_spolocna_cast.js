6700749e15851435ca59015c433317fb1d2e9ad9
matej <matejkrc1@gmail.com>
2018-12-27 20:12:00 +0100

    Pridane zobrazovanie poslednych stranok

--- JS/krc_spolocna_cast.js
+++ JS/krc_spolocna_cast.js
@@ -14,3 +14,71 @@ function pageLoadCounter() {
 function clearLocalStorage(){
     localStorage= null;
 }
+
+/* Kontrola ci podporuje localStorage */
+function supports_html5_storage() {
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /* Prida a zaroven zobrazuje posledne navstivene stranky */
+function lastPages(){
+    if (!supports_html5_storage())
+        {console.log("supportsLocalStorage nie je podporovany"); return; }
+    console.log("supportsLocalStorage je podporovany");
+    if(localStorage.length == 0)
+        {for(let i = 0; i < 5 ; i++) {localStorage[i] = 'undefined'}};
+
+    var i = 0;
+    for (i = 0; i < 5; i++) {
+        if(localStorage[i] == 'undefined'){
+            localStorage[i] = window.location.href;
+            i = 6;
+        }
+    }
+    if(i == 5){
+        for (i = 0; i< 4;i++){
+            localStorage[i] = localStorage[i+1];
+        }
+        localStorage[i] = window.location.href;
+    }
+    
+    var put = document.getElementById('past');
+
+    for(i = 0; i < 5 ; i++){
+        if(localStorage[i] == 'undefined'){
+            break;
+        }
+        else{
+            let a = document.createElement('a');
+
+            let endIndex = localStorage[i].indexOf('.html');
+            let sub = localStorage[i].substring(0,endIndex);
+            let startIndex = sub.lastIndexOf('/');
+            sub = localStorage[i].substring(startIndex+1,endIndex);
+            let linkText = null;
+            if(localStorage[i+1] == 'undefined' | i == localStorage.length -1 ){
+                linkText = document.createTextNode(sub);
+            }
+            else{
+                linkText = document.createTextNode(sub + " -> ");
+            }
+            a.appendChild(linkText);
+            a.title = localStorage[i];
+            a.href = localStorage[i];
+            put.appendChild(a);
+
+            a = null;
+        }
+    }
+    console.log('nacital som');
+    console.log(pages);
+}
+
+/* Maze local storage */
+function clearStorage(){
+    localStorage.clear();
+}
